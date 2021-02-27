7720e663e3028e208e6f1c9d01830e14a1f53206
Ian Dela Cruz <iandc76@gmail.com>
2021-02-27 10:46:38 +0800

    Create tests Fixes #8

---
+++ tests/integration/convert.test.js
@@ -0,0 +1,18 @@
+import fetch from 'node-fetch';
+import input from '../fixtures/input.json';
+
+describe('HTML to PDF', () => {
+  it('should convert html to PDF', async () => {
+    expect.assertions(1);
+    const res = await fetch('http://localhost:3000', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(input),
+    });
+
+    const json = await res.json();
+    expect(json.data).toContain('JVBERi0xLjQKMSAwIG9iago8PAovVGl0bGUgKP7');
+  });
+});
